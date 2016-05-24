dd2c09c63c226cd43d9aa48a6057d807b6642813
Austin Gilbert <agilbert39@gatech.edu>
2016-05-24 16:30:45 -0500

    Modified the directoryObject for speed, made major file navigation improvements
    --HG--
    branch : dev

--- acquisition.py
+++ acquisition.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from PySide import QtCore
 from PySide import QtGui
@@ -24,6 +25,14 @@ source:String
     a string denoting where the current directoryObject is from.
     A future goal of this application is the ability to support
     data from multiple sources via ssh.
+
+Notes:
+-------
+I have not investigated it deeply due to my ignorance regarding networking,
+but the following topics may provide the foundation needed to build an inter-device
+application:
+-python subprocess module
+-QtGui.QFileSystemModel
 """
 
 class DirectoryObject(object):
@@ -41,23 +50,23 @@ class DirectoryObject(object):
 
     r"""Provides a list of information about the current working directory"""
     def make_directory_tree(self):
-        storageList = []
-        for root, dirs, files in os.walk(self.location):
-            root = root.split('/')
-            dirName = root[-1]
-            dirRepresentation = [dirName, dirs, files]
-            storageList.append(dirRepresentation)
-        return storageList[0]
+        a = os.walk(self.location).next()
+        dirName = a[0].split('/')
+        dirName = dirName[-1]
+        return [dirName, a[1], a[2]]
 
     r"""gets your location"""
     def get_location(self):
         return self.location
 
+    def get_top_dir_name(self):
+        return self.directoryTree[0]
+
     r"""gets the subdirectories present at your location."""
     def get_sub_directories(self):
         return self.directoryTree[1]
 
-    r"""gets teh files at your location."""
+    r"""gets the files at your location."""
     def get_files(self):
         return self.directoryTree[2]
 
@@ -88,15 +97,83 @@ class DirectoryObject(object):
             self.location = os.getcwd()
             self.directoryTree = self.make_directory_tree()
 
+r"""A widget for adding sources, finding files, and passing said files to the
+load command.
+
+"""
 class acquisitionSourceW(QtGui.QWidget):
 
     def __init__(self):
         super(acquisitionSourceW, self).__init__()
+
         self.directoryObjs = []
-        self.availableSourcesW = QtGui.QWidget()
-        self.addButton = QtGui.QPushButton()
-        self.removeButton = QtGui.QPushButton()
-        self.rArrow = QtGui.QPushButton()
-        self.lArrow = QtGui.QPushButton()
-        self.leftClickMenuBar = QtGui.QMenuBar()
-        self.aspectRatio = 5
+        self.make_initial_DirectoryObjects()
+
+        self.activeDirectoryObj = self.directoryObjs[0]
+        self.activeSource = self.activeDirectoryObj.source
+
+        self.fileTreeWidget = QtGui.QTreeWidget()
+        self.set_file_tree_widget()
+        self.fileTreeWidget.itemCollapsed.connect(self.move_up)
+        self.fileTreeWidget.itemClicked.connect(self.move_down)
+
+        self.lButton = QtGui.QPushButton("<")
+        self.rButton = QtGui.QPushButton(">")
+        self.sourceLabel = QtGui.QLabel("Source: " + self.activeSource)
+
+        self.sourceBarLayout = QtGui.QHBoxLayout()
+        self.sourceBarLayout.addWidget(self.lButton)
+        self.sourceBarLayout.addWidget(self.sourceLabel)
+        self.sourceBarLayout.addWidget(self.rButton)
+
+        self.sourceBarWidget = QtGui.QWidget()
+        self.sourceBarWidget.setLayout(self.sourceBarLayout)
+
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(self.sourceBarWidget)
+        self.layout.addWidget(self.fileTreeWidget)
+
+        self.setLayout(self.layout)
+        self.show()
+
+    def make_initial_DirectoryObjects(self):
+        a = DirectoryObject()
+        self.directoryObjs.append(a)
+
+    def set_file_tree_widget(self):
+        self.fileTreeWidget.setColumnCount(1)
+        hiddenHeader = QtGui.QTreeWidgetItem()
+        hiddenHeader.setText(0, "go away")
+        self.fileTreeWidget.setHeaderItem(hiddenHeader)
+        hiddenHeader.setHidden(True)
+        self.fileTreeWidget.setHeaderItem(None)
+        directory = QtGui.QTreeWidgetItem(self.fileTreeWidget)
+        directory.setText(0, self.activeDirectoryObj.get_top_dir_name())
+        for x in self.activeDirectoryObj.get_files():
+            childItem = QtGui.QTreeWidgetItem(directory)
+            childItem.setText(0, x)
+        for x in self.activeDirectoryObj.get_sub_directories():
+            childItem = QtGui.QTreeWidgetItem(directory)
+            childItem.setText(0, x)
+        self.fileTreeWidget.addTopLevelItem(directory)
+        directory.setExpanded(True)
+
+    def move_up(self):
+        self.fileTreeWidget.clear()
+        self.activeDirectoryObj.change_directory(direction = -1)
+        self.set_file_tree_widget()
+
+    def move_down(self):
+        if self.fileTreeWidget.currentItem().text(0) in self.activeDirectoryObj.get_sub_directories():
+            nextDir = self.fileTreeWidget.currentItem().text(0)
+            self.fileTreeWidget.clear()
+            self.activeDirectoryObj.change_directory(direction = 1, path = nextDir)
+            self.set_file_tree_widget()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = acquisitionSourceW()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
