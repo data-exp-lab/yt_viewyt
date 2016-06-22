562fdf36fbac96277a432b9f3edcebe7a898aeef
Austin Gilbert <agilbert39@gatech.edu>
2016-06-22 16:28:56 -0500

    Switched from actual plots to an frb object.
    --HG--
    branch : dev

--- yt_viewyt/visualization.py
+++ yt_viewyt/visualization.py
@@ -1,14 +1,20 @@
 import yt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 class YtPlot(object):
 
     def __init__(self, data_source):
         super(YtPlot, self).__init__()
         self.source = data_source
-        self.plot = yt.ProjectionPlot(data_source, "x", 'density', center = [0.5, 0.5, 0.5])
-        self.plot = self.plot.plots[('gas', 'density')]
-        self.plot = FigureCanvas(self.plot.figure)
+        self.frb = self.source.r[:, :, 0.5].to_frb(1, 1024)
+        self.fieldVal = self.frb["density"].ndarray_view()
+        fig = Figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.imshow(self.fieldVal)
+        self.plot = FigureCanvas(fig)
+
+
 
     def get_plot(self):
         return self.plot
